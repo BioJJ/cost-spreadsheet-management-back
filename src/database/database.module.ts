9b711526bb2ae1710ba28a62ad897578a3c62513
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-10-04 20:17:22 -0400

    feat: ✨ add database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module, Global } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { SequelizeModule } from '@nestjs/sequelize'
+
+@Global()
+@Module({
+	imports: [
+		SequelizeModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				dialect: 'postgres',
+				type: 'postgres',
+				host: configService.get('POSTGRES_HOST'),
+				port: configService.get('POSTGRES_PORT'),
+				username: configService.get('POSTGRES_USER'),
+				password: configService.get('POSTGRES_PASSWORD'),
+				database: configService.get('POSTGRES_DB'),
+				synchronize: true,
+				autoLoadModels: true
+			})
+		})
+	],
+	exports: [SequelizeModule]
+})
+export class DatabaseModule {}
